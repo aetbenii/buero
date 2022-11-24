d7b0393a1514821340110c03d5d9917b8032291d
beni <benny.fcb89@gmail.com>
2022-11-24 10:49:27 +0100

    Initial commit

---
+++ src/main/java/java_db/Employee.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_db;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Employee {
+    private int id;
+    private String name;
+    private String role;
+    private Date insertTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getInsertTime() {
+        return insertTime;
+    }
+
+    public void setInsertTime(Date insertTime) {
+        this.insertTime = insertTime;
+    }
+}
